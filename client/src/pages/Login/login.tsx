0fcdd1ed35b701cd46494500a6b7c4096d8a074f
kianlak <lak.kian.ca@gmail.com>
2023-03-10 00:31:05 -0600

    Frontend Completed (Before Ansuhka Wireframes)

--- client/src/pages/Login/login.tsx
+++ client/src/pages/Login/login.tsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
-import { Box, Paper, TextField, Button, Grid } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, Paper, TextField, Button, Alert } from '@mui/material';
 import "../Login/login.css";
-import backgroundImage from "../img/BackgroundImage.png";
+import backgroundImage from "./img/LoginBackground.png";
 
 export const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  useEffect(() => {
+    let timer: string | number | NodeJS.Timeout | undefined;
+    if (showErrorMessage) {
+      timer = setTimeout(() => {
+        setError('');
+        setShowErrorMessage(false);
+      }, 5000);
+    }
+    return () => clearTimeout(timer);
+  }, [showErrorMessage]);
 
   const handleLogin = () => {
     if (username.trim() === '' || password.trim() === '') {
       setError('Please enter username and password');
+      setShowErrorMessage(true);
     } else {
       // handle login logic here
       console.log(`Logged in with username: ${username} and password: ${password}`);
@@ -18,40 +31,37 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <Box className="loginBackground" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <Paper className="loginPaper" elevation={20} sx={{ borderRadius: 5 }}>
-        <div className="loginWrapper">
-          <Grid item xs={12}>
-            <h1 className="loginHeader">Welcome</h1>
-          </Grid>
-          <Grid item xs={12}>
+    <Box className="login-background" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <Paper className="login-paper" elevation={20} sx={{ borderRadius: 5 }}>
+        <div className="login-wrapper">
+          <h3 className="login-header">Welcome</h3>
+          <TextField
+            className="login-textfield"
+            label="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <div>
             <TextField
-              className="loginTextfield"
-              label="Username"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              className="loginTextfield"
+              className="login-textfield"
               label="Password"
               type="password"
               value={password}
               onChange={(event) => setPassword(event.target.value)}
             />
-          </Grid>
-          <div className="loginButtonWrapperRow">
-            <Grid item xs={8}>
-              <Button variant="contained" color="primary" onClick={handleLogin}>
-                Login
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <a href="#">Forgot Password?</a>
-            </Grid>
+            <p>Forgot password?</p>
           </div>
-          {error && <p>{error}</p>}
+
+          <div className="login-button-wrapper">
+            <Button className="login-button" variant="outlined" color="primary">
+              Sign Up
+            </Button>
+
+            <Button className="login-button" variant="contained" color="primary" onClick={handleLogin}>
+              Login
+            </Button>
+          </div>
+          {showErrorMessage && <Alert variant="filled" severity="error">{error}</Alert>}
         </div>
       </Paper>
     </Box>
