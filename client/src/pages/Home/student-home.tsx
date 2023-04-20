898ff2770d628222a6c8e493cd5a61d220d6e224
Sammy Mahmoudi <sammymahmoudi@gmail.com>
2023-04-20 16:53:13 -0500

    kianlakoogaming

--- client/src/pages/Home/student-home.tsx
+++ client/src/pages/Home/student-home.tsx
@@ -1,39 +1,53 @@
-import * as React from 'react';
+import * as React from "react";
 import { Navbar } from "../../components/navbar/navbar";
-import { Calendar } from './components/calendar/calendar';
-import { Typography, Box } from '@mui/material';
-import { StudentAppointments } from './components/student-appointments/student-appointments';
+import { Calendar } from "./components/calendar/calendar";
+import { Typography, Box } from "@mui/material";
+import { StudentAppointments } from "./components/student-appointments/student-appointments";
 import "../Home/student-home.css";
 
 export const StudentHome: React.FC = () => {
   const [selectedDate, setSelectedDate] = React.useState<Date | null>(null);
 
-  return(
+  return (
     <div>
-      <div className='top-content'>
+      <div className="top-content">
         <Navbar />
-        <div className='container'>
-        
+        <div className="container">
           {/* Calendar */}
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Calendar onDateChange={(date: Date | null) => setSelectedDate(date)} />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Calendar
+              onDateChange={(date: Date | null) => setSelectedDate(date)}
+            />
           </Box>
 
           {/* Appointments */}
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             {selectedDate ? (
               <Typography variant="h4">
                 <StudentAppointments />
                 {new Date(selectedDate).toLocaleDateString()}
               </Typography>
             ) : (
-              <Typography variant="h4">No date selected</Typography>
+              <Typography variant="h4">
+                <StudentAppointments />
+                No date selected
+              </Typography>
             )}
           </Box>
-
         </div>
       </div>
     </div>
-    
   );
 };
